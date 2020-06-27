0479a2108228d75eb338db277c8f9a29c54d6c18
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-27 04:44:46 +0300

    Java code added

---
+++ src/main/java/ru/netology/Statistic.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+public class Statistic {
+
+        public long findMax(long[] incomes){
+            long currentMaxIndex = 0;
+            long currentMax = incomes[0];
+            for (long income : incomes)
+                if (currentMax < income)
+                    currentMax = income;
+            return currentMax;
+        }
+        public long calculateSum(long[] incomes){
+            long sum = 0;
+            for (long income : incomes) {
+                sum += income;
+            }
+            return sum;
+        }
+
+        public long calculateAverage(long[] incomes){
+            long average = calculateSum(incomes) / incomes.length;
+
+            return average;
+        }
+    }
+
